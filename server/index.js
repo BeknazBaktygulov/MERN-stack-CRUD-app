4a497d2a9acfc78a0105768b500f09d651f2dd99
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-12 10:54:43 -0700

    Demo of app.get and

--- server/index.js
+++ server/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const FoodModel = require('./models/Food')
 
 // middleware
 app.use(express.json)
 
 mongoose.connect('mongodb+srv://admin:Outtasystem9!@cluster0.k2u6p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
+})
+
+app.get('/',  async (req, res) => {
+    const food = new FoodModel({foodName: "Apple", daySinceIAte: 3  });
+    try {
+        await food.save();
+    } catch(err) {
+        console.log(err);
+    }
 })
 
 app.listen(4000, () => {
