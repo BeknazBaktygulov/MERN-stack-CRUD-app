38205273493c233489b9e6dc91d3d0ee3f151a02
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-12 10:39:55 -0700

    Developed server side with schema

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 // middleware
 app.use(express.json)
 
-mongoose.connect('', {
+mongoose.connect('mongodb+srv://admin:Outtasystem9!@cluster0.k2u6p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 })
 
